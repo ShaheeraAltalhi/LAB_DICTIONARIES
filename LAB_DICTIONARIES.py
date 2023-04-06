aa7d509863e6c5de172c3d83552f3777ccd30f0c
shaheera Talhi <sh.talhi@gmail.com>
2023-04-06 13:54:56 +0300

    phone book

---
+++ LAB_DICTIONARIES.py
@@ -0,0 +1,22 @@
+#Q1:
+Names = ["Amal", "Mohammed", "Khadijah", "Abdullah", "Rawan", "Faisal", "Layla"]
+Numbers = ["0568323222", "0522222232", "0545341144", "0545534556", "0560664566", "0567917077"]
+Phone_book = {"Number" : Numbers, "Name" : Names }
+
+number = input(" Enter the phone Number")
+
+if not number.isdigit():
+    print("This is invalid number, use only number.")
+elif:
+    print("sorry, the phone number is not found")
+elif len(number) < or len(number) > 10:
+    print("This is invalis number")
+else:
+    print("The phone Number is for:", Phone_book[number])
+
+
+
+
+#Q2:
+List = [5, 0, 34, 9, 0, 13, 8]
+NewList = a
